65c8fa32331a10ed7b5206362185ec12fe2c6827
HP <lazovic.dusan.bb@gmail.com>
2023-04-06 22:27:47 +0200

    core app added. db configured.

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': '127.0.0.1',
+        'NAME': 'devdb',
+        'USER': 'postgres',
+        'PASSWORD': 'test1',
+        'PORT': '5432',
     }
 }
 
